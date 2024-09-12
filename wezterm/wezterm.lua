48f3ec8969b8f35896c517a67bab47169549bf24
TJ Smith <tj.smith@salesloft.com>
2024-09-12 10:03:04 -0700

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,26 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd)
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 return {
-  font = wezterm.font "FiraCode Nerd Font Mono",
-  font_size = 15,
-  color_scheme = 'Dracula (Official)',
-  window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW",
-  enable_tab_bar = false,
-  initial_rows = 50,
-  initial_cols = 200,
-  audible_bell="Disabled",
-  send_composed_key_when_left_alt_is_pressed=true,
+	font = wezterm.font_with_fallback({
+		{ family = "FiraCode Nerd Font Mono", weight = "Bold" },
+		"Material Icons",
+	}),
+	font_size = 15,
+	color_scheme = "Dracula (Official)",
+	window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW",
+	use_fancy_tab_bar = false,
+	window_background_opacity = 0.9,
+	enable_tab_bar = true,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	initial_rows = 50,
+	initial_cols = 200,
+	audible_bell = "Disabled",
+	send_composed_key_when_left_alt_is_pressed = true,
 }
